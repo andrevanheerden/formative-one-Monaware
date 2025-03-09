0be684d72a80a0c4472e7a21a6f0f25d702724ec
andrevanheerden <160416636+andrevanheerden@users.noreply.github.com>
2025-03-09 21:37:32 +0200

    InfoCard base

--- monaware/src/Components/Home.js
+++ monaware/src/Components/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
+import InfoCard from "../Components/InfoCard";
 
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <p>Welcome to the Home page!</p>
+      <InfoCard />
     </div>
   );
 }
--- monaware/src/Components/InfoCard.js
+++ monaware/src/Components/InfoCard.js
@@ -1,13 +1,54 @@
+import React from 'react';
 import Card from 'react-bootstrap/Card';
+import "../App.css";
 
-function TextExample() {
+/* static data */
+import AdultBlackDragon from '../Assets/staticData/AdultBlackDragon.png';
+
+const InfoCard = () => {
+  const cardDetails = [
+    {
+      Image: AdultBlackDragon,
+      Title: "AdultBlack Dragon",
+      size: "Huge",
+      type: "Dragon",
+      alignment: "chaotic evil",
+      damageVulnerabilities: "none",
+      damageResistances: "none",
+      damageImmunities: "acid",
+      languages: "common draconic",
+      challengeRating: "14",
+      XP: "11500"
+    }
+  ];
+
+  const displayCard = (card, index) => {
+    return (
+      <Card key={index}>
+        <Card.Body className='InfoCardBody'>
+         <Card.Img variant="top" src={card.Image} alt="Card image" className='InfoCardImg' />
+          <Card.Title className='InfoCardTitle'>{card.Title}</Card.Title>
+          <Card.Text className='InfoCardText'>
+            <p>Size : {card.size}</p>
+            <p>Type : {card.type}</p>
+            <p>Alignment : {card.alignment}</p>
+            <p>Damage Vulnerabilities : {card.damageVulnerabilities}</p>
+            <p>Damage Resistances : {card.damageResistances}</p>
+            <p>Damage Immunities : {card.damageImmunities}</p>
+            <p>Languages : {card.languages}</p>
+            <p>Challenge Rating : {card.challengeRating}</p>
+            <p>XP : {card.XP}</p>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  };
+
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-       
-      </Card.Body>
-    </Card>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {cardDetails.map((card, index) => displayCard(card, index))}
+    </div>
   );
-}
+};
 
-export default TextExample;
+export default InfoCard;
